19780011cd1b0d956165f23fb8ad1e0668ea26a2
Tharuka Saranga Jayawardena <163109273+sarangaviii@users.noreply.github.com>
2024-11-26 23:16:57 +0530

    Vendor class updated

--- backend/real-time-event-ticketing-system/src/main/java/com/tharuka/real_time_event_ticketing_system/model/Vendor.java
+++ backend/real-time-event-ticketing-system/src/main/java/com/tharuka/real_time_event_ticketing_system/model/Vendor.java
@@ -3,4 +3,27 @@ package com.tharuka.real_time_event_ticketing_system.model;
 public class Vendor {
     private int vendorId;
     private String vendorName;
+
+    public Vendor(int vendorId, String vendorName) {
+        this.vendorId = vendorId;
+        this.vendorName = vendorName;
+    }
+
+    public int getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(int vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+
 }
