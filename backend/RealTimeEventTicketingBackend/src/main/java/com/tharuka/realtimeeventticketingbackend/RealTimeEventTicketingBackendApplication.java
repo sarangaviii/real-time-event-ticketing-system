1debf691685db8fed755cf0769df148c3700603f
Tharuka Saranga Jayawardena <163109273+sarangaviii@users.noreply.github.com>
2024-12-02 14:43:47 +0530

    Replace and updated backend files to fix an error

---
+++ backend/RealTimeEventTicketingBackend/src/main/java/com/tharuka/realtimeeventticketingbackend/RealTimeEventTicketingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tharuka.realtimeeventticketingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeEventTicketingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeEventTicketingBackendApplication.class, args);
+    }
+
+}
---
+++ backend/RealTimeEventTicketingBackend/src/test/java/com/tharuka/realtimeeventticketingbackend/RealTimeEventTicketingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tharuka.realtimeeventticketingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeEventTicketingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
