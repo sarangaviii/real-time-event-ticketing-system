31f375417410d17718118ae7bf19cda92fb2fe86
Tharuka Saranga Jayawardena <163109273+sarangaviii@users.noreply.github.com>
2024-12-03 21:57:01 +0530

    Updated CustomerService, VendorService.

--- backend/RealTimeEventTicketingBackend/src/main/java/com/tharuka/realtimeeventticketingbackend/service/CustomerService.java
+++ backend/RealTimeEventTicketingBackend/src/main/java/com/tharuka/realtimeeventticketingbackend/service/CustomerService.java
@@ -1,4 +1,28 @@
-package main.java.com.tharuka.realtimeeventticketingbackend.service;
+package com.tharuka.realtimeeventticketingbackend.service;
 
-public class CustomerService {
+import com.tharuka.realtimeeventticketingbackend.model.TicketPool;
+
+public class CustomerService implements Runnable{
+    private final TicketPool ticketPool;
+    private final int cutomerId;
+    private final int ticketPerSecond;
+
+    public CustomerService(TicketPool ticketPool, int cutomerId, int ticketPerSecond) {
+        this.ticketPool = ticketPool;
+        this.cutomerId = cutomerId;
+        this.ticketPerSecond = ticketPerSecond;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                int ticket = ticketPool.removeTicket();
+                System.out.println("Customer " + cutomerId + " purchased Ticket " + ticket);
+                Thread.sleep(1000 / ticketPerSecond);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
--- backend/RealTimeEventTicketingBackend/src/main/java/com/tharuka/realtimeeventticketingbackend/service/VendorService.java
+++ backend/RealTimeEventTicketingBackend/src/main/java/com/tharuka/realtimeeventticketingbackend/service/VendorService.java
@@ -1,4 +1,35 @@
-package main.java.com.tharuka.realtimeeventticketingbackend.service;
+package com.tharuka.realtimeeventticketingbackend.service;
 
-public class VendorService {
+import com.tharuka.realtimeeventticketingbackend.model.TicketPool;
+
+public class VendorService implements Runnable {
+    private final TicketPool ticketPool;
+    private final int vendorId;
+    private final int ticketsPerSecond;
+    private final int totalTickets;
+    public static int ticketCounter = 1;
+
+    public VendorService(TicketPool ticketPool, int vendorId, int ticketsPerSecond, int totalTickets) {
+        this.ticketPool = ticketPool;
+        this.vendorId = vendorId;
+        this.ticketsPerSecond = ticketsPerSecond;
+        this.totalTickets = totalTickets;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (ticketCounter <= totalTickets) { // Loop until all tickets are added
+                synchronized (VendorService.class) {
+                    if (ticketCounter <= totalTickets) { // Check if all tickets have been added
+                        ticketPool.addTickets(ticketCounter++);
+                        System.out.println("Vendor " + vendorId + " added Ticket " + (ticketCounter - 1));
+                    }
+                }
+                Thread.sleep(1000 / ticketsPerSecond); // Sleep for 1 second
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
