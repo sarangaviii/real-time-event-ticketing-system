8aa15f891563aa5f7775086fa54a3226db4084e0
Tharuka Saranga Jayawardena <163109273+sarangaviii@users.noreply.github.com>
2024-12-07 00:25:59 +0530

    Removed @Service annotations

--- backend/RealTimeEventTicketingBackend/src/main/java/com/tharuka/realtimeeventticketingbackend/service/CustomerService.java
+++ backend/RealTimeEventTicketingBackend/src/main/java/com/tharuka/realtimeeventticketingbackend/service/CustomerService.java
@@ -4,15 +4,15 @@ import com.tharuka.realtimeeventticketingbackend.model.TicketPool;
 import org.springframework.stereotype.Service;
 
 
-@Service
+
 public class CustomerService implements Runnable{
     private final TicketPool ticketPool;
-    private final int cutomerId;
+    private final int customerId;
     private final int ticketPerSecond;
 
-    public CustomerService(TicketPool ticketPool, int cutomerId, int ticketPerSecond) {
+    public CustomerService(TicketPool ticketPool, int customerId, int ticketPerSecond) {
         this.ticketPool = ticketPool;
-        this.cutomerId = cutomerId;
+        this.customerId = customerId;
         this.ticketPerSecond = ticketPerSecond;
     }
 
@@ -21,7 +21,7 @@ public class CustomerService implements Runnable{
         try {
             while (true) {
                 int ticket = ticketPool.removeTicket();
-                System.out.println("Customer " + cutomerId + " purchased Ticket " + ticket);
+                System.out.println("Customer " + customerId + " purchased Ticket " + ticket);
                 Thread.sleep(1000 / ticketPerSecond);
             }
         } catch (InterruptedException e) {
