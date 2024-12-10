0b77dd50e9be345b35eca30e17038241c9d9b312
Tharuka Saranga Jayawardena <163109273+sarangaviii@users.noreply.github.com>
2024-12-10 17:15:46 +0530

    Coded App.js

--- frontend/react-frontend/src/App.js
+++ frontend/react-frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ConfigurationForm from './components/ConfigurationForm';
+import SystemStatus from "./components/SystemStatus";
+import ControlPanel from './components/ControlPanel';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+    <div>
+      <h1>Real-Time Event Ticketing</h1>
+      <ConfigurationForm />
+      <SystemStatus />
+      <ControlPanel />
     </div>
-  );
-}
+);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
